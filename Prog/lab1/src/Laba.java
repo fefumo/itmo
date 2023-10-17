e8534bedf2b19fd24a852e20e6c10043c39696d9
fefumo <77743898+fefumo@users.noreply.github.com>
2023-10-17 19:41:40 +0300

    Update Laba.java

--- Prog/lab1/src/Laba.java
+++ Prog/lab1/src/Laba.java
@@ -1,15 +1,14 @@
-import java.util.Arrays;
+
 import java.lang.Math;
-import java.util.List;
 
 public class Laba {
+
     public static void main(String[] args) {
-        int[] c = new int[9]; 
+        int[] d = new int[9]; 
         
         for (int i = 0; i < 9; i++){ // 18 16 14 12 10 8 6 4 2
-            c[i] = 18 - i*2;  
+            d[i] = 18 - i*2;  
         }
-        //System.out.println(Arrays.toString(c));
 
         double[] x = new double[18];
 
@@ -18,11 +17,27 @@ public class Laba {
             x[i] = -14.0 + (double)(Math.random() * ((13.0 - (-14.0)) + 1));
         }
 
-        //System.out.println(Arrays.toString(x));
         
+
         double[][] doubles = new double[9][18];
 
-        //List<Integer> list = Arrays.asList(4,8,14,18);
+
+        init_doubles(d, x, doubles);
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 18; j++){
+                System.out.printf("%-10.5f ", doubles[i][j]);
+            }
+            System.out.printf("\n");
+        }
+        
+    }
+
+    public static void f(int[] args) {
+        args[0] = 0;
+    
+    }
+
+    public static void init_doubles(int[] c, double[] x, double[][] doubles){
 
         for (int i = 0; i < doubles.length; i++){
             for (int j = 0; j < doubles[i].length; j++){
@@ -35,33 +50,10 @@ public class Laba {
                     doubles[i][j] = Math.pow(0.5/a2,3);
                 }
                 else{
-                    doubles[i][j] = (3.0/4.0) * ( (1 - Math.cbrt(Math.cos(x[j]))) / 4);
+                    doubles[i][j] = (3.0/4) * ( (1 - Math.cbrt(Math.cos(x[j]))) / 4);
                 }
             }
         }
-        // for (int i = 0; i < doubles.length; i++){
-        //     for (int j = 0; j < doubles[i].length; j++){
-        //         double p1, p2, p3, p4, p5, p6, p7, p8 ,p9;
-        //         p1 = Math.asin((x[j] - 0.5)/27);
-        //         p2 = Math.pow((x[j] -1), 3);
-        //         p3 = Math.PI + p2;
-        //         p4 = x[j] * x[j];
-        //         p5 = Math.pow(p4, p3);
-        //         p6 = Math.PI + p5;
-        //         p7 = 0.5 / p6;
-        //         p8 = Math.pow(p7, 3);
-                
-        //         System.out.printf("%.5f %.5f %.5f %.5f %.5f %.5f %.5f %.5f", p1, p2, p3, p4, p5, p6, p7, p8);
-        //         break;
-        //System.out.println(Arrays.deepToString(doubles));
-
-        for (int i = 0; i < 9; i++){
-            for (int j = 0; j < 18; j++){
-                System.out.printf("%-10.5f ", doubles[i][j]);
-            }
-            //System.out.println();
-            System.out.printf("\n");
-        }
-
-        }
     }
+
+}
